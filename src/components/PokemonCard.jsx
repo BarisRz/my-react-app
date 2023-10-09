48f7cf2aa1431082b2ba7f1b2a8761112e3438a3
Baris G <barisg@pm.me>
2023-10-09 22:47:37 +0200

    props quete

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,14 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+/* eslint-disable react/prop-types */
+function PokemonCard(props) {
+  console.log(props);
+  const { pokemon } = props;
   return (
-    <figure>
-      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>????</p>}
+    <figure className="card">
+      {pokemon.imgSrc ? (
+        <img className="card-img" src={pokemon.imgSrc} />
+      ) : (
+        <p>????</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
